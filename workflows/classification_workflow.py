9799d2fb6016f60d98f89722669275b805dc523b
linus <lagbleze@st.ug.edu.gh>
2025-03-28 23:49:18 +0100

    fix: add imports

--- workflows/classification_workflow.py
+++ workflows/classification_workflow.py
@@ -4,3 +4,14 @@ from dataclasses import dataclass
 from typing import Tuple
 
 import joblib
+import pandas as pd
+from dataclasses_json import dataclass_json
+from flytekit import Resources, task, workflow
+from flytekit.types.file import FlyteFile
+from flytekit.types.schema import FlyteSchema
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+
+
