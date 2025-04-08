c67b19fe989c771a3f98a02f7b9d2618b071ab32
linus <lagbleze@st.ug.edu.gh>
2025-04-08 23:59:11 +0200

    feat: add fit

--- workflows/classification_workflow.py
+++ workflows/classification_workflow.py
@@ -72,7 +72,14 @@ def fit(x: FlyteSchema[FEATURE_COLUMNS],
         ) -> model_file:
     x_df = x.open().all()
     y_df = y.open().all()
-    m = XGBClassifier(
-                    n_jobs=hyperparams.n_jobs,
-                    max_depth=hyperparams.max_depth
-    )
+    m = XGBClassifier(n_jobs=hyperparams.n_jobs,
+                    max_depth=hyperparams.max_depth,
+                    n_estimators=hyperparams.n_estimators,
+                    booster=hyperparams.booster,
+                    objective=hyperparams.objective,
+                    learning_rate=hyperparams.learning_rate
+                    )
+    m.fit(x_df, y_df)
+    fname = "model.joblib.dat"
+    joblib.dump(m, fname)
+    return (fname,)
