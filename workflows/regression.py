e8ba368adbd3a6792f16813b85aa3e44ab520caf
linus <lagbleze@st.ug.edu.gh>
2025-04-19 23:30:09 +0200

    fix: add imports

--- workflows/regression.py
+++ workflows/regression.py
@@ -1,7 +1,8 @@
-from flytekit import Resources, dynamic, workflow
+from flytekit import Resources, dynamic, workflow, task
 from typing import Tuple
-import pandas
+import pandas as pd
 import numpy as np
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-
+import joblib
+from flytekit.types.file import JoblibSerializedFile
