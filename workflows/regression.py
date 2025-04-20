8cfc536b19a1e3c357580e207d29f49c00f278c2
linus <lagbleze@st.ug.edu.gh>
2025-04-20 23:36:52 +0200

    feat: add feature names

--- workflows/regression.py
+++ workflows/regression.py
@@ -6,3 +6,14 @@ from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 import joblib
 from flytekit.types.file import JoblibSerializedFile
+
+
+
+NUM_HOUSES_PER_LOCATION = 1000
+COLUMNS = [
+    "PRICE", "YEAR_BUILT", "SQUARE_FEET",
+    "NUM_BEDROOMS", "NUM_BATHROOMS",
+    "LOT_ACRES", "GARAGE_SPACES"
+]
+MAX_YEAR = 2021
+
